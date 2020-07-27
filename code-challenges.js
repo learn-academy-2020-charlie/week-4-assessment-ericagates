fcd9f3fb2db29c0371fbbc7e4a2a77ddd7d0e298
ericagates <ericajoangates@gmail.com>
2020-07-26 23:39:40 -0700

    assessement complete

--- code-challenges.js
+++ code-challenges.js
@@ -7,9 +7,37 @@ var collections = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example2 (can be a different order): ["blue", "green", "pink"]
 // Expected output example3 (can be a different order): ["pink", "green"]
 
+// create a function that takes in an array
+const shuffleRemaining = (array) => {
+    // if the array is empty, return "the array is empty"
+    if (array.length === 0){
+        return "The array is empty."
+    } else {
+        // create a new array to store updated array
+        let newArray = array
+        // remove the first item from the array
+        newArray.shift()
+        // shuffle the remaining items in the array
+        // starting from the last index in the array, and going down
+        for(let i = newArray.length - 1; i > 0; i--){
+            // create and store a random number to identify the index that will be swapped with the current index
+            let j = Math.floor(Math.random() * i)
+            // temporarily store the number in the current index
+            let temp = newArray[i]
+            // replace the number in current index with the number in index j
+            newArray[i] = newArray[j]
+            // replace the number in index j with the number stored in temp
+            newArray[j] = temp
+          }
+        return newArray
+    }
+}
 
-
-
+let collections2  = shuffleRemaining(collections)
+console.log(collections2)
+let collections3 = shuffleRemaining(collections2)
+console.log(collections3)
+console.log(shuffleRemaining(collections3))
 
 // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -18,7 +46,20 @@ var cubeAndSum1 = [2, 3, 4]
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+// create a function that takes in an array
+const cubeSum = (array) => {
+    // create a variable to store each item in the array cubed, and add it together
+    let sumCubed = 0
+    // map through each item in the array and turn it into a number cube it, and add it to sumCubed
+    array.map(value => {
+        sumCubed += parseInt(value)**3  
+    })
+    return sumCubed
+}
 
+
+console.log(cubeSum(cubeAndSum1))
+console.log(cubeSum(cubeAndSum2))
 
 
 
@@ -29,6 +70,42 @@ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
 
+// create a function that takes an array of numbers
+const minAndMax = (array) => {
+    // create a new array to store min and max number
+    let newArray = []
+    //initially set the minimum and max values to the first item in the array
+    let minValue = array[0]
+    let maxValue = array[0]
+    
+    // filter through each item in array to find if it's less than the current min value
+    array.filter(value => {
+        if (value < minValue){
+            // if current value is less than the min value reset the min value
+            minValue = value
+        }
+        return minValue
+    })
+
+    // filter through each item in array to find if it's greater than the current max value
+    array.filter(value => {
+        if (value > maxValue){
+            // if current value is less than the max value reset the max value
+            maxValue = value
+        }
+        return maxValue
+    })
+
+    //push min value to new array
+    newArray.push(minValue)
+    //push  max value to new array
+    newArray.push(maxValue)
+
+    return newArray
+}
+
+console.log(minAndMax(nums1))
+console.log(minAndMax(nums2))
 
 
 
@@ -40,6 +117,21 @@ var testString1 = "albatross"
 var testString2 = "jabberwocky"
 // Expected output: "jAbBeRwOcKy"
 
+// create a function that takes in a string
+const oddCaps = (string) =>{
+    let newString = string
+    // loop through the string odd indexes of the string
+    for (let i = 1; i < newString.length; i = i+2) {
+        // update the character with the odd index Capitalized
+        newChar = newString.charAt(i).toUpperCase()
+        // add the new character by slicing the string at the index location
+        newString = newString.slice(0, i) + newChar + newString.slice(i+1)
+    }
+    return newString
+}
+
+console.log(oddCaps(testString1))
+console.log(oddCaps(testString2))
 
 
 
@@ -49,3 +141,28 @@ var testString2 = "jabberwocky"
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+//create a function that accepts 2 arrays
+const noDupes = (array1, array2) => {
+    let noDupesArray = []
+
+    //map through first array and make sure there are no duplicates
+    array1.map((value, index) => {
+        // if the current index is the first occurrence (or first index) of the value in the array, add it to the noDupesArray
+        if (array1.indexOf(value) === index) {
+            return noDupesArray.push(value) 
+        }
+    })
+
+    //map through second array and compare to no Dupes Array
+    array2.map(value => {
+        // if the current value does not have an index (doesn't exist = -1) in the noDupesArray, add it to the array
+        if (noDupesArray.indexOf(value)  < 0) {
+            return noDupesArray.push(value)
+             
+        }
+    })
+    return noDupesArray
+}
+
+console.log(noDupes(arr1,arr2))
--- interview-questions.md
+++ interview-questions.md
@@ -7,48 +7,60 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is an instance variable in Ruby? How is it different from other variables?
 
-  Your answer:
+  Your answer: An instance variable in Ruby is a variable that starts with @ and is scoped to a class where it's defined and any other inherited classes. Any objects that are created from that class can have different instances of that variable but they are different from other variables because they can only be accessed by the object.
 
-  Researched answer:
+  Researched answer: In the Ruby programming language, an instance variable is a type of variable which starts with an @ symbol. An instance variable is used as part of Object-Oriented Programming (OOP) to give objects their own private space to store data. https://www.rubyguides.com/2019/07/ruby-instance-variables/
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: A block is a defined portion of code like a function that starts with a do or { and ends with an end or } and outputs the result of the function.
 
   Researched answer:
-
+  A Ruby block is a way of grouping statements, and may appear only in the source adjacent to a method call; the block is written starting on the same line as the method call's last parameter (or the closing parenthesis of the parameter list) http://rubylearning.com/satishtalim/ruby_blocks.html
 
 
 3. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: An implicit return means that you do not need to do define what a function will return. Ruby automatically returns the last line in the function.
 
   Researched answer:
-
+   A function that returned values without using the return keyword. Any statement in Ruby returns the value of the last evaluated expression https://www.freecodecamp.org/news/idiomatic-ruby-writing-beautiful-code-6845c830c664
 
 
 4. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: Object-oriented programming is the concept of creating programs where every object belongs to a class with data and behavior. Function programming is the concept of using functions with behavior to create a program.
 
-  Researched answer:
+  Researched answer: Functional programming and object-oriented programming uses a different method for storing and manipulating the data. In functional programming, data cannot be stored in objects and it can only be transformed by creating functions. In object-oriented programming, data is stored in objects https://www.educba.com/functional-programming-vs-oop/
 
 
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: A class is a blueprint for an object and is the intersection of data and behaviors. The object is an instance of the class.
 
-  Researched answer:
+  Researched answer:  A class is a template for objects. An object is a member or an "instance" of a class. An object has a state in which all of its properties have values that you either explicitly define or that are defined by default settings. https://www.ncl.ucar.edu/Document/HLUs/User_Guide/classes/classoview.shtml
 
 
 
 ## Looking Ahead: Terms for Next Week
 - PostgreSQL
+also known as Postgres, is a free and open-source relational database management system (RDBMS) emphasizing extensibility and SQL compliance.https://www.educba.com/sql-server-vs-postgresql/
+
 - API
+a set of functions and procedures allowing the creation of applications that access the features or data of an operating system, application, or other service https://en.wikipedia.org/wiki/Application_programming_interface
+
 - CRUD
+In computer programming, create, read, update, and delete (CRUD) are the four basic functions of persistent storage. https://en.wikipedia.org/wiki/Create,_read,_update_and_delete
+
 - Ruby on Rails
+Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller framework, providing default structures for a database, a web service, and web pages https://en.wikipedia.org/wiki/Ruby_on_Rails
+
 - ORM
+Object-relational mapping (ORM) is a mechanism that makes it possible to address, access and manipulate objects without having to consider how those objects relate to their data sources. https://searchwindevelopment.techtarget.com/definition/object-relational-mapping
+
+
 - Active Record
+Active Record is the M in MVC - the model - which is the layer of the system responsible for representing business data and logic. Active Record facilitates the creation and use of business objects whose data requires persistent storage to a database.
